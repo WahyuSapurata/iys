81792aa9bd4207d7391641840fa36739dc78bcfd
WahyuSapurata <wm337708@gmail.com>
2024-03-08 10:29:52 +0800

    fix

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -1419,6 +1419,25 @@
                                                     </span>
                                                     <!--end::Svg Icon-->
                                                     <span
+                                                        class="fw-semibold fs-6 text-white opacity-75 text-start pe-3">Bilateral
+                                                        Forum</span>
+                                                </div>
+                                                <!--end::Item-->
+                                                <!--begin::Item-->
+                                                <div class="d-flex gap-4 mb-5">
+                                                    <!--begin::Svg Icon | path: icons/duotune/general/gen043.svg-->
+                                                    <span class="svg-icon svg-icon-1 svg-icon-white">
+                                                        <svg width="24" height="24" viewBox="0 0 24 24"
+                                                            fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                            <rect opacity="0.3" x="2" y="2" width="20"
+                                                                height="20" rx="10" fill="currentColor" />
+                                                            <path
+                                                                d="M10.4343 12.4343L8.75 10.75C8.33579 10.3358 7.66421 10.3358 7.25 10.75C6.83579 11.1642 6.83579 11.8358 7.25 12.25L10.2929 15.2929C10.6834 15.6834 11.3166 15.6834 11.7071 15.2929L17.25 9.75C17.6642 9.33579 17.6642 8.66421 17.25 8.25C16.8358 7.83579 16.1642 7.83579 15.75 8.25L11.5657 12.4343C11.2533 12.7467 10.7467 12.7467 10.4343 12.4343Z"
+                                                                fill="currentColor" />
+                                                        </svg>
+                                                    </span>
+                                                    <!--end::Svg Icon-->
+                                                    <span
                                                         class="fw-semibold fs-6 text-white opacity-75 text-start pe-3">Social
                                                         Night</span>
                                                 </div>
@@ -1737,8 +1756,8 @@
                 <!--begin::Heading-->
                 <div class="text-center mb-17">
                     <!--begin::Title-->
-                    <h3 class="fs-2hx text-dark mb-5" id="clients" data-kt-scroll-offset="{default: 125, lg: 150}">
-                        ANNOUNCEMENT</h3>
+                    {{-- <h3 class="fs-2hx text-dark mb-5" id="clients" data-kt-scroll-offset="{default: 125, lg: 150}">
+                        ANNOUNCEMENT</h3> --}}
                     <!--end::Title-->
                 </div>
                 <!--end::Heading-->
